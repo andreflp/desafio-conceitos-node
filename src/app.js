e7f2c8579cfb7eca7109587d68da5a810751c133
André Felipe <flpandresilva@gmail.com>
2020-06-16 00:03:01 -0300

    ADD: implementação das rotas da API

--- src/app.js
+++ src/app.js
@@ -1,33 +1,81 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors")
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4")
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
 app.get("/repositories", (request, response) => {
-  // TODO
-});
+  response.json(repositories)
+})
 
 app.post("/repositories", (request, response) => {
-  // TODO
-});
+  const { title, url, techs } = request.body
+
+  const repository = { 
+    id: uuid(), 
+    title, 
+    url, 
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository)
+
+  response.json(repository)
+})
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' })
+  }
+
+  const likes = repositories[repositoryIndex].likes
+  const repository = { id, title, url, techs, likes }
+
+  repositories[repositoryIndex] = repository
+
+  response.json(repository)
+})
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' })
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+  response.status(204).send()
+
+})
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' })
+  }
+  const repository = repositories[repositoryIndex]
+
+  repository.likes++
+
+  response.json(repository)
+})
 
 module.exports = app;
